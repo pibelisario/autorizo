3f54914bb0a8b20e7b71bcbad361a9e83c025e5e
Paulo Inácio <108241856+pibelisario@users.noreply.github.com>
2023-03-23 16:55:09 -0300

    Initial commit

---
+++ src/main/java/caixa/beneficente/autorizo/AutorizoApplication.java
@@ -0,0 +1,14 @@
+package caixa.beneficente.autorizo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutorizoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutorizoApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/caixa/beneficente/autorizo/AutorizoApplicationTests.java
@@ -0,0 +1,13 @@
+package caixa.beneficente.autorizo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutorizoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
