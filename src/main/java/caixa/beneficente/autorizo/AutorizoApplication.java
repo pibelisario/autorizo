ec7261d6ea82e70c514b923dc5ad20224c663871
Paulo Inácio <108241856+pibelisario@users.noreply.github.com>
2023-04-14 20:04:46 -0300

    gerando fragmento

--- src/main/java/caixa/beneficente/autorizo/AutorizoApplication.java
+++ src/main/java/caixa/beneficente/autorizo/AutorizoApplication.java
@@ -4,6 +4,7 @@ import java.io.FileNotFoundException;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.lowagie.text.DocumentException;
 
@@ -13,7 +14,7 @@ public class AutorizoApplication {
 	public static void main(String[] args) throws DocumentException, FileNotFoundException {
 		SpringApplication.run(AutorizoApplication.class, args);
 
-		// System.out.println(new BCryptPasswordEncoder().encode("123"));
+		System.out.println(new BCryptPasswordEncoder().encode("123"));
 
 	}
 
