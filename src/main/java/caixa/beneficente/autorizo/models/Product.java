21c4be7bbc6b35c26cb6eb606859e36d8287badc
Paulo Inácio <108241856+pibelisario@users.noreply.github.com>
2023-04-19 17:13:29 -0300

    adicionando classe teste

---
+++ src/main/java/caixa/beneficente/autorizo/models/Product.java
@@ -0,0 +1,13 @@
+package caixa.beneficente.autorizo.models;
+
+public class Product {
+
+    String nome;
+    Double price;
+
+    public Product(String nome, Double price) {
+        this.nome = nome;
+        this.price = price;
+    }
+
+}
--- src/main/java/caixa/beneficente/autorizo/util/testeClass.java
+++ src/main/java/caixa/beneficente/autorizo/util/testeClass.java
@@ -1,7 +1,18 @@
 package caixa.beneficente.autorizo.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import caixa.beneficente.autorizo.models.Product;
+
 public class testeClass {
     public static void main(String[] args) {
-        System.out.println("teste");
+
+        List<Product> list = new ArrayList<>();
+        list.add(new Product("Tv", 900.00));
+        list.add(new Product("Mouse", 50.00));
+        list.add(new Product("Tablet", 350.50));
+        list.add(new Product("HD Case", 80.90));
     }
+
 }
